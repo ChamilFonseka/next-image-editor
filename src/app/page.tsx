7644e29b28ba690ece6e508275c5bb4abd0d1165
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-03-12 21:17:24 +0530

    added the PhotoUploader component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import { Editor } from "@/components/Editor";
+import PhotoUploader from "@/components/PhotoUploader";
 
 export default function Home() {
   return (
-    <main className="
-      min-h-dvh
-      max-w-screen-lg mx-auto
-      flex flex-col items-center justify-center 
-      bg-white
-      p-4
-      ">
-      <Editor />
+    <main className="min-h-dvh flex flex-col items-center justify-center p-8">
+      <PhotoUploader />
     </main>
   );
 }
---
+++ src/components/PhotoUploader.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { Image as ImageIcon, MousePointerSquareDashed } from "lucide-react";
+import { useState } from "react";
+import Dropzone, { FileRejection } from "react-dropzone";
+import { toast } from "sonner";
+
+const PhotoUploader = () => {
+
+    const [isDragOver, setIsDragOver] = useState<boolean>(false);
+
+    const onDropRejected = (rejectedFiles: FileRejection[]) => {
+        const [file] = rejectedFiles;
+
+        setIsDragOver(false);
+
+        toast.error(`${file.file.type} type is not supported.`, {
+            description: "Please choose a PNG, JPG, or JPEG image instead.",
+        });
+    };
+
+    const onDropAccepted = (acceptedFiles: File[]) => {
+        const [file] = acceptedFiles;
+
+        setIsDragOver(false);
+
+        toast.success(`File ${file.name} uploaded successfully!`);
+    };
+
+    return (
+        <Dropzone
+            onDropRejected={onDropRejected}
+            onDropAccepted={onDropAccepted}
+            accept={{
+                'image/png': ['.png'],
+                'image/jpeg': ['.jpeg'],
+                'image/jpg': ['.jpg'],
+            }}
+            onDragEnter={() => setIsDragOver(true)}
+            onDragLeave={() => setIsDragOver(false)}>
+            {({ getRootProps, getInputProps }) => (
+                <div
+                    style={{ background: "linear-gradient(180deg,#f3f1ff 10.1%,#fff4ee 126.67%)" }}
+                    className='flex-1 rounded-3xl flex flex-col items-center justify-center w-full h-full'
+                    {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    {isDragOver ? (
+                        <MousePointerSquareDashed className='size-32 text-zinc-500 mb-2' />
+                    ) : (
+                        <ImageIcon className='size-32 text-zinc-500 mb-2' />
+                    )}
+                    <div className='flex flex-col justify-center mb-2 text-sm text-zinc-700'>
+                        {isDragOver ? (
+                            <p>
+                                <span className='font-semibold'>Drop file</span> to upload
+                            </p>
+                        ) : (
+                            <p>
+                                <span className='font-semibold'>Click to upload</span> or drag and drop
+                            </p>
+                        )}
+                    </div>
+                    <p className='text-xs text-zinc-500'>PNG, JPG, JPEG</p>
+                </div>
+            )}
+        </Dropzone>
+    );
+};
+export default PhotoUploader;    
